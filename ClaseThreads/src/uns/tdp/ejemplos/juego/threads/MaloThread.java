225aa3d476779e5e9161da61287d8354b78a517b
juan_trippel <juan.trippel@gmail.com>
2015-10-15 14:19:00 -0300

    Limpieza general.

--- ClaseThreads/src/uns/tdp/ejemplos/juego/threads/MaloThread.java
+++ ClaseThreads/src/uns/tdp/ejemplos/juego/threads/MaloThread.java
@@ -8,7 +8,8 @@ public class MaloThread extends Thread {
 	private Malo mLogica;
 	
 	// Flag que indica cuando debe detenerse la ejecución del hilo.
-	private boolean mDetener;
+	// Es volatile porque es accedida desde concurrentemente desde diferentes threads.
+	private volatile boolean mDetener;
 	
 	public MaloThread(Malo logica) {
 		this.mLogica  = logica;
@@ -19,26 +20,28 @@ public class MaloThread extends Thread {
 	public void run() {
 		// Ejecuto indefinidamente hasta que el flag sea verdadero.
 		while(!this.mDetener){
-			// Realizo el movimiento
-			this.mLogica.mover();
-			
 			// Duermo el hilo 1 segundo.
 			// De esta manera cada turno se ejecuta cada 1 segundo.
 			try {
 				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+				
+				// Realizo el movimiento
+				this.mLogica.mover();
+			} catch (InterruptedException e) { }
 		}
 	}
 	
-	
-	public void destruir() {
+	public void detener() {
 		// Interrumpo el hilo para que no continue con su ejecución.
-		Thread.currentThread().interrupt();
+		this.interrupt(); 
 		
 		// Seteamos el flag para detener su ejecución.
 		this.mDetener = true;
+	}
+	
+	public void destruir() {
+		// Detengo la ejecucion del hilo.
+		this.detener();
 		
 		// Notificamos a la logica que este hilo se destruyo.
 		this.mLogica.destruir();
